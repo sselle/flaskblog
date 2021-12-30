0400106ed45591ebf2c77bbf69d6e7ec6d3c1cc9
Sven Selle <s.selle@web.de>
2021-12-30 11:16:07 +0100

    added debugging in vscode

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-default-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, '/db/app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
