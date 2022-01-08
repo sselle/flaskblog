62f7b7f18ebffbb218b3af264cb8d1570ab1b9f3
Sven Selle <s.selle@web.de>
2022-01-08 16:48:50 +0100

    create logging to file, fixed bug with username change

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -12,3 +16,17 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
+
+if not app.debug:
+ 
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
